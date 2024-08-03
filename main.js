c05f1b678a83506555552663046aabbc29aef2ec
Carlos <ferbenjamin03@gmail.com>
2024-08-03 17:28:51 -0500

    agregando animacion de nave espacial

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const container = document.querySelector(".contenedor");
+const container = document.querySelector(".contenedor1");
 const fragmento = document.createDocumentFragment();
 
-const colorRGB = () => {
+const colorHexadecimal = () => {
     let array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
     let color = "";
     for (let i = 0; i < 3; i++) {
@@ -13,21 +13,21 @@ const colorRGB = () => {
     return color;
 }
 
-var n = prompt("Ingrese la cantidad de circulos: ");
-for (let i = 0; i < n; i++) {
+
+for (let i = 0; i < 5; i++) {
     let div = document.createElement("DIV");
     div.classList.add("circulo");
-    let color = colorRGB();
+    let color = colorHexadecimal();
     div.style.backgroundColor = `#${color}`;    
     let image = document.createElement("IMG");
     image.setAttribute("src", "img/mundo.png");
-    image.classList.add("word-image");
+    image.classList.add("world-image");
     div.appendChild(image);
     fragmento.appendChild(div);
 }
 container.appendChild(fragmento);
-const circulo = document.querySelectorAll (".circulo");
-circulo.forEach((elemento, indice) => {
+const circulos = document.querySelectorAll (".circulo");
+circulos.forEach((elemento, indice) => {
     elemento.addEventListener('click', () => {
         alert (`Este es el elemento ${indice+1}`);
     })
