c8125fb425a3c0c044851214586d82c7d295b285
Carlos <ferbenjamin03@gmail.com>
2024-08-03 19:38:34 -0500

    actualizacion aparecer circulos con js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const boton = document.getElementById("button");
+const input = document.getElementById("input");
 const container = document.querySelector(".contenedor-circle");
-const fragmento = document.createDocumentFragment();
 
 const colorHexadecimal = () => {
     let array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
@@ -14,32 +13,33 @@ const colorHexadecimal = () => {
     return color;
 }
 
-boton.addEventListener('click', () => {
-    if (container.hasChildNodes() === false) {
-        let valor = boton.previousElementSibling.value;
-        for (let i = 0; i < valor; i++) {
-            var div = document.createElement("DIV");
-            div.classList.add("circulo");
-            var color = colorHexadecimal();
-            div.style.backgroundColor = `#${color}`;    
-            var image = document.createElement("IMG");
-            image.setAttribute("src", "img/mundo.png");
-            image.classList.add("world-image");
-            div.appendChild(image);
-            fragmento.appendChild(div);
+const inputFuction = () => {
+    if (container.hasChildNodes() === true) {
+        let cantidadCircle = container.childElementCount;
+        for(let i = 0; i < cantidadCircle; i++) {
+            container.removeChild(container.firstElementChild);
         }
-        container.appendChild(fragmento);
-        const circulos = document.querySelectorAll(".circulo")
-        circulos.forEach((elemento, indice) => {
-            elemento.addEventListener('click', () => {
-                alert(`Este es el elemento ${indice+1}`);
-            })
-        })
-    }   else {
-        container.childNodes.forEach((elemento) => {
-            container.removeChild(elemento);
-        })
-
     }
-})
-
+    let fragmento = document.createDocumentFragment();
+    let valor = parseInt(input.value);
+    for (let i = 0; i < valor; i++) {
+        var div = document.createElement("DIV");
+        div.classList.add("circulo");
+        var color = colorHexadecimal();
+        div.style.backgroundColor = `#${color}`;    
+        var image = document.createElement("IMG");
+        image.setAttribute("src", "img/mundo.png");
+        image.classList.add("world-image");
+        div.appendChild(image);
+        fragmento.appendChild(div);
+    }
+    container.appendChild(fragmento);
+    const circulos = document.querySelectorAll(".circulo")
+    circulos.forEach((elemento, indice) => {
+        elemento.addEventListener('click', () => {
+            alert(`Este es el elemento ${indice+1}`);
+        })
+    })
+}
+input.nextElementSibling.addEventListener('click', inputFuction);
+input.addEventListener('keypress', inputFuction);
